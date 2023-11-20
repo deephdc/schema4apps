c991324abf914b984ab1f5c03b0a2800f8128184
Alvaro Lopez Garcia <aloga@ifca.unican.es>
2023-11-20 23:00:22 +0100

    Make code black

--- ai4metav/main.py
+++ ai4metav/main.py
@@ -6,7 +6,8 @@ from jsonschema import Draft7Validator
 from jsonschema import draft7_format_checker
 
 
-SCHEMA = os.path.join(os.path.dirname(__file__), 'schemata/ai4-apps.json')
+SCHEMA = os.path.join(os.path.dirname(__file__), "schemata/ai4-apps.json")
+
 
 def load_json(f):
     data = f.read()
@@ -14,22 +15,22 @@ def load_json(f):
 
 
 def validate():
-    parser = argparse.ArgumentParser(description=('DEEP application metadata '
-                                                  '(JSON-schema based) '
-                                                  'validator.'))
-    parser.add_argument('instance',
-                        metavar='METADATA_JSON',
-                        type=argparse.FileType('r'),
-                        nargs='+',
-                        help='DEEP application metadata')
+    parser = argparse.ArgumentParser(
+        description=("DEEP application metadata " "(JSON-schema based) " "validator.")
+    )
+    parser.add_argument(
+        "instance",
+        metavar="METADATA_JSON",
+        type=argparse.FileType("r"),
+        nargs="+",
+        help="DEEP application metadata",
+    )
     args = parser.parse_args()
 
-    with open(SCHEMA, 'r') as f:
+    with open(SCHEMA, "r") as f:
         schema = load_json(f)
     Draft7Validator.check_schema(schema)
 
     for f in args.instance:
         instance = load_json(f)
-        Draft7Validator(
-            schema,
-            format_checker=draft7_format_checker).validate(instance)
+        Draft7Validator(schema, format_checker=draft7_format_checker).validate(instance)
